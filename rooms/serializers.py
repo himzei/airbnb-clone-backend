e44ea62efe9e2ddfd1b0ecfc45152057ab75845c
조현일 <himzei@gmail.com>
2022-09-20 02:07:25 +0900

    11.4

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,15 +1,37 @@
 from dataclasses import field
 from rest_framework.serializers import ModelSerializer
 from .models import Amenity, Room
+from users.serializers import TinyUserSerializer
+from categories.serializers import CategorySerializer
 
 class AmenitySerializer(ModelSerializer):
   class Meta: 
     model = Amenity 
+    fields = ("name", "description")
+
+
+class RoomDetailSerializer(ModelSerializer):
+
+  owner = TinyUserSerializer()
+  amenities = AmenitySerializer(many=True)   
+  category = CategorySerializer()      
+
+  class Meta: 
+    model = Room 
     fields = "__all__"
 
 
-class RoomSerializer(ModelSerializer): 
+class RoomListSerializer(ModelSerializer): 
   class Meta: 
     model = Room
-    fields = "__all__"
-    depth = 1
+    fields = (
+      "pk", 
+      "name", 
+      "country", 
+      "city", 
+      "price"
+    )
+
+
+
+
